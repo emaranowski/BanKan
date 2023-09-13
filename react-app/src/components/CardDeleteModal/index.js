48c5be087d1f6620a0d024a48ad101f3ad73d274
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2023-09-12 21:44:47 -0700

    Update cardOrder on Col when deleting Card

--- react-app/src/components/CardDeleteModal/index.js
+++ react-app/src/components/CardDeleteModal/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { thunkDeleteCard } from "../../store/cards";
-import { thunkGetAllColumnsForBoard } from '../../store/columns';
+import { thunkUpdateColumn, thunkGetAllColumnsForBoard } from '../../store/columns';
 import "./CardDeleteModal.css";
 
-export default function CardDeleteModal({ cardId, boardId }) {
+export default function CardDeleteModal({ card, column, boardId }) {
   const dispatch = useDispatch();
   const { closeModal } = useModal();
+  const cardId = card.id;
+  const cardOrderArr = column.cardOrder.split(',');
 
   const handleDelete = async (e) => {
     e.preventDefault();
@@ -15,8 +17,46 @@ export default function CardDeleteModal({ cardId, boardId }) {
     try {
       const res = await dispatch(thunkDeleteCard(cardId));
       if (res.message) {
+
+        console.log('**** in DELETE CARD TRY, RES OK res:', res)
+        console.log('**** in DELETE CARD TRY, RES OK res.message:', res.message)
+        console.log('**** in DELETE CARD TRY, RES OK cardOrderArr:', cardOrderArr)
+
+        const idxOfDeletedCard = cardOrderArr.indexOf(card.dndId);
+        console.log('**** in DELETE CARD TRY, RES OK idxOfDeletedCard:', idxOfDeletedCard)
+
+        console.log('**** in DELETE CARD TRY, RES OK cardOrderArr:', cardOrderArr)
+        if (cardOrderArr.length === 1) {
+          cardOrderArr.splice(0, 1); // remove 1 at idx 0
+          cardOrderArr.splice(0, 0, ''); // remove 0, add '' at idx 0
+        } else if (cardOrderArr.length > 1) {
+          cardOrderArr.splice(idxOfDeletedCard, 1); // remove 1 at idxOfDeletedCard
+          // cardOrderArr.splice(idxOfDeletedCard, 0, ''); // remove 0, add '' at idxOfDeletedCard
+        }
+        console.log('**** in DELETE CARD TRY, RES OK cardOrderArr:', cardOrderArr)
+
+        // console.log('**** in DELETE CARD TRY, RES OK cardOrderArr:', cardOrderArr)
+        // cardOrderArr.splice(idxOfDeletedCard, 1); // remove 1 at idxOfDeletedCard
+        // cardOrderArr.splice(idxOfDeletedCard, 0, ''); // remove 0, add '' at idxOfDeletedCard
+        // console.log('**** in DELETE CARD TRY, RES OK cardOrderArr:', cardOrderArr)
+
+        console.log('**** in DELETE CARD TRY, RES OK column.cardOrder:', column.cardOrder)
+        const cardOrderUpdatedStr = cardOrderArr.toString();
+        console.log('**** in DELETE CARD TRY, RES OK cardOrderUpdatedStr:', cardOrderUpdatedStr)
+
+        const columnUpdated = {
+          ...column,
+          cardOrder: cardOrderUpdatedStr,
+        };
+
+        console.log('**** in DELETE CARD TRY, RES OK column:', column)
+        console.log('**** in DELETE CARD TRY, RES OK columnUpdated:', columnUpdated)
+
+        dispatch(thunkUpdateColumn(columnUpdated))
+
+
         closeModal();
-        dispatch(thunkGetAllColumnsForBoard(boardId));
+        // dispatch(thunkGetAllColumnsForBoard(boardId));
       }
     } catch {
       closeModal();
