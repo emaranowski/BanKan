ff01ee4c57543940dfd2cfea8b3011aebef17168
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2023-09-14 12:58:51 -0700

    Home: CSS, content

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -1,44 +1,59 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
-// import { Link } from 'react-router-dom';
-// import OpenModalButton from '../OpenModalButton';
-// import SignupFormModal from '../SignupFormModal';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import OpenModalButton from '../OpenModalButton';
+import SignupFormModal from '../SignupFormModal';
 import './Home.css'
 
 export default function Home() {
-  // const sessionUser = useSelector(state => state.session.user)
+  const sessionUser = useSelector(state => state.session.user)
 
   return (
     <>
-      <div id='homeBanner'>
-        <div id='homeBannerBg'>
-          {/* <img
-						id='homeBannerImg'
+      <div id='home-banner-bg'>
+        {/* <img
+						id='home-banner-img'
 						src=""
 					/> */}
-        </div>
-        <div id='homeBannerText'>
-          <div id='homeHeader'>
+      </div>
+
+      <div id='home-content'>
+        <div id='home-banner-text'>
+          <div id='home-header'>
             BanKan brings all your<br></br>
             tools and tasks together
           </div>
-          <div id='homeSubHeader'>
+          <div id='home-sub-header'>
             Keep it all in one place
           </div>
-          {/* <div id='homeSignupBtn'>
-						Sign up for free
-					</div> */}
+          {!sessionUser ?
+            <div id='home-signup-btn'>
+              <OpenModalButton
+                buttonText="Sign up - it's free!"
+                modalComponent={<SignupFormModal />}
+              />
+            </div>
+            :
+            <Link to='/boards'>
+              <button id='home-my-boards-btn'>
+                Go to my boards
+              </button>
+            </Link>
+          }
+        </div>
 
-          {/* {!sessionUser ?
-						<div id='homeSignupBtn'>
-							<OpenModalButton
-								buttonText="Sign up - it's free!"
-								modalComponent={<SignupFormModal />}
-							/>
-						</div>
-						: null
-					} */}
-
+        <div id='home-content-productivity'>
+          <div id='home-bankan-101'>
+            BANKAN 101
+          </div>
+          <div id='home-productivity'>
+            Power your productivity
+          </div>
+          <div id='home-productivity-text'>
+            Streamlined and minimalistic.
+            Create dynamic boards. Jot down notes. Check things off your lists.
+            Cut the clutter with just what you need. Get started in seconds.
+          </div>
         </div>
       </div>
     </>
