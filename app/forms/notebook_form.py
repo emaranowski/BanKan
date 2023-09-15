864984fb5b7180279ca2e75e17983792b0990235
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2023-09-15 15:26:58 -0700

    Notebook: add to migration, edit form

--- app/forms/notebook_form.py
+++ app/forms/notebook_form.py
@@ -4,6 +4,6 @@ from wtforms.validators import DataRequired, URL, Length
 
 class NotebookForm(FlaskForm):
     user_id = IntegerField('user_id', validators=[DataRequired()])
-    image_url = StringField('image_url', validators=[DataRequired(), URL()])
+    image_url = StringField('image_url', validators=[DataRequired(), URL(), Length(min=1, max=255)])
     title = StringField('title', validators=[DataRequired(), Length(min=1, max=50)])
     note_order = StringField('note_order')
