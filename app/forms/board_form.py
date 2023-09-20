b3dabcfbe3ff1e9b56bb2d5d6aeaad77613e371c
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2023-09-20 13:13:39 -0700

    board_form: add Length validation and message

--- app/forms/board_form.py
+++ app/forms/board_form.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class BoardForm(FlaskForm):
     user_id = IntegerField('user_id', validators=[DataRequired()])
     image_url = StringField('image_url', validators=[DataRequired()])
-    title = StringField('title', validators=[DataRequired()])
+    title = StringField('title', validators=[DataRequired(), Length(min=1, max=30, message='Title must be 1-30 characters')])
