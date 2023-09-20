c3f877c1c417c4dc01578cf08a8c8622c8b2a114
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2023-09-20 10:48:29 -0700

    LoginFormPage: CSS, add Demo User btn

---
+++ react-app/src/components/NoteForm/colorNames.js
@@ -0,0 +1,54 @@
+const colorNames = [
+    {
+        'id': 1,
+        // 'hex': '#a11800',
+        'name': 'red',
+    },
+    {
+        'id': 2,
+        // 'hex': '#a15600',
+        'name': 'orange',
+    },
+    {
+        'id': 3,
+        // 'hex': '#b08307',
+        'name': 'yellow',
+    },
+    {
+        'id': 4,
+        // 'hex': '#3a8501',
+        'name': 'green',
+    },
+    {
+        'id': 5,
+        // 'hex': '#016285',
+        'name': 'blue',
+    },
+    {
+        'id': 6,
+        // 'hex': '#450185',
+        'name': 'purple',
+    },
+    {
+        'id': 7,
+        // 'hex': '#cecece',
+        'name': 'lightgray',
+    },
+    {
+        'id': 8,
+        // 'hex': '#8f8f8f',
+        'name': 'medgray',
+    },
+    {
+        'id': 9,
+        // 'hex': '#686868',
+        'name': 'darkgray',
+    },
+    {
+        'id': 10,
+        // 'hex': '#000000',
+        'name': 'black',
+    }
+]
+
+export default colorNames;
