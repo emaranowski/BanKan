d5ab5726c7b2ff357ed44689b8cda969fbcfd705
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2023-11-27 19:44:36 -0800

    note form & model: cleanup

--- app/models/note.py
+++ app/models/note.py
@@ -1,13 +1,16 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
+
 class Note(db.Model):
-    __tablename__ = 'notes'
+    __tablename__ = "notes"
 
     if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+        __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    notebook_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('notebooks.id')), nullable=False)
+    notebook_id = db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("notebooks.id")), nullable=False
+    )
     color_name = db.Column(db.String(30), nullable=False)
     title = db.Column(db.String(30), nullable=False)
     text = db.Column(db.String(200))
@@ -15,18 +18,17 @@ class Note(db.Model):
     updated_at = db.Column(db.Date, nullable=False)
 
     def to_dict(self):
-
-        dnd_id = 'note-'+str(self.id)
+        dnd_id = "note-" + str(self.id)
 
         return {
-            'id': self.id,
-            'notebookId': self.notebook_id,
-            'colorName': self.color_name,
-            'title': self.title,
-            'text': self.text,
-            'dndId': dnd_id,
-            'createdAt': self.created_at,
-            'updatedAt': self.updated_at
+            "id": self.id,
+            "notebookId": self.notebook_id,
+            "colorName": self.color_name,
+            "title": self.title,
+            "text": self.text,
+            "dndId": dnd_id,
+            "createdAt": self.created_at,
+            "updatedAt": self.updated_at,
         }
 
     # one-to-many: one notebook can have many notes
