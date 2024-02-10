8b2e1e41a01ed6aa1fbbb01d1944a0cd58174420
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2024-02-10 13:48:16 -0800

    Format dunder init dot py

--- app/__init__.py
+++ app/__init__.py
@@ -5,21 +5,21 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 from .models import db, User
-from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.board_routes import board_routes
-from .api.column_routes import column_routes
 from .api.card_routes import card_routes
-from .api.notebook_routes import notebook_routes
+from .api.column_routes import column_routes
 from .api.note_routes import note_routes
+from .api.notebook_routes import notebook_routes
+from .api.user_routes import user_routes
 from .seeds import seed_commands
 from .config import Config
 
-app = Flask(__name__, static_folder='../react-app/build', static_url_path='/')
+app = Flask(__name__, static_folder="../react-app/build", static_url_path="/")
 
 # Setup login manager
 login = LoginManager(app)
-login.login_view = 'auth.unauthorized'
+login.login_view = "auth.unauthorized"
 
 
 @login.user_loader
@@ -31,13 +31,13 @@ def load_user(id):
 app.cli.add_command(seed_commands)
 
 app.config.from_object(Config)
-app.register_blueprint(user_routes, url_prefix='/api/users')
-app.register_blueprint(auth_routes, url_prefix='/api/auth')
-app.register_blueprint(board_routes, url_prefix='/api/boards')
-app.register_blueprint(column_routes, url_prefix='/api/columns')
-app.register_blueprint(card_routes, url_prefix='/api/cards')
-app.register_blueprint(notebook_routes, url_prefix='/api/notebooks')
-app.register_blueprint(note_routes, url_prefix='/api/notes')
+app.register_blueprint(auth_routes, url_prefix="/api/auth")
+app.register_blueprint(board_routes, url_prefix="/api/boards")
+app.register_blueprint(card_routes, url_prefix="/api/cards")
+app.register_blueprint(column_routes, url_prefix="/api/columns")
+app.register_blueprint(note_routes, url_prefix="/api/notes")
+app.register_blueprint(notebook_routes, url_prefix="/api/notebooks")
+app.register_blueprint(user_routes, url_prefix="/api/users")
 db.init_app(app)
 Migrate(app, db)
 
@@ -52,9 +52,9 @@ CORS(app)
 # Well.........
 @app.before_request
 def https_redirect():
-    if os.environ.get('FLASK_ENV') == 'production':
-        if request.headers.get('X-Forwarded-Proto') == 'http':
-            url = request.url.replace('http://', 'https://', 1)
+    if os.environ.get("FLASK_ENV") == "production":
+        if request.headers.get("X-Forwarded-Proto") == "http":
+            url = request.url.replace("http://", "https://", 1)
             code = 301
             return redirect(url, code=code)
 
@@ -62,12 +62,12 @@ def https_redirect():
 @app.after_request
 def inject_csrf_token(response):
     response.set_cookie(
-        'csrf_token',
+        "csrf_token",
         generate_csrf(),
-        secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
-        samesite='Strict' if os.environ.get(
-            'FLASK_ENV') == 'production' else None,
-        httponly=True)
+        secure=True if os.environ.get("FLASK_ENV") == "production" else False,
+        samesite="Strict" if os.environ.get("FLASK_ENV") == "production" else None,
+        httponly=True,
+    )
     return response
 
 
@@ -76,26 +76,31 @@ def api_help():
     """
     Returns all API routes and their doc strings
     """
-    acceptable_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
-    route_list = { rule.rule: [[ method for method in rule.methods if method in acceptable_methods ],
-                    app.view_functions[rule.endpoint].__doc__ ]
-                    for rule in app.url_map.iter_rules() if rule.endpoint != 'static' }
+    acceptable_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    route_list = {
+        rule.rule: [
+            [method for method in rule.methods if method in acceptable_methods],
+            app.view_functions[rule.endpoint].__doc__,
+        ]
+        for rule in app.url_map.iter_rules()
+        if rule.endpoint != "static"
+    }
     return route_list
 
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def react_root(path):
     """
     This route will direct to the public directory in our
     react builds in the production environment for favicon
     or index.html requests
     """
-    if path == 'favicon.ico':
-        return app.send_from_directory('public', 'favicon.ico')
-    return app.send_static_file('index.html')
+    if path == "favicon.ico":
+        return app.send_from_directory("public", "favicon.ico")
+    return app.send_static_file("index.html")
 
 
 @app.errorhandler(404)
 def not_found(e):
-    return app.send_static_file('index.html')
+    return app.send_static_file("index.html")
