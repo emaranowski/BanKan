fe6ff7eee2de0b75351b4ee907b326070046120d
Erica Maranowski <128769940+emaranowski@users.noreply.github.com>
2024-02-18 12:46:36 -0800

    Refactor reducers: boards.js

--- react-app/src/store/boards.js
+++ react-app/src/store/boards.js
@@ -161,35 +161,51 @@ export default function boardsReducer(state = initialState, action) {
   switch (action.type) {
 
     case GET_ONE_BOARD: {
-      const newState = { ...state, oneBoard: {} };
-      newState.oneBoard = action.board;
-      return newState;
+      return {
+        ...state,
+        oneBoard: action.board,
+      };
     }
 
     case GET_ALL_BOARDS: {
-      const newState = { ...state, allBoards: {} };
-      action.boards.boards.forEach((boardObj) => {
-        newState.allBoards[boardObj.id] = boardObj
-      });
-      return newState;
+      return {
+        ...state,
+        allBoards: action.boards.boards.reduce((acc, board) => {
+          acc[board.id] = board;
+          return acc;
+        }, { ...state.allBoards })
+      };
     }
 
     case CREATE_BOARD: {
-      const newState = { ...state };
-      newState.allBoards[action.board.id] = action.board;
-      return newState;
+      return {
+        ...state,
+        allBoards: {
+          ...state.allBoards,
+          [action.board.id]: action.board,
+        },
+      };
     }
 
     case UPDATE_BOARD: {
-      const newState = { ...state, oneBoard: {} };
-      newState.allBoards[action.board.id] = action.board;
-      return newState;
+      return {
+        ...state,
+        oneBoard: {},
+        allBoards: {
+          ...state.allBoards,
+          [action.board.id]: action.board,
+        },
+      };
     }
 
     case DELETE_BOARD: {
-      const newState = { ...state, oneBoard: {}, allBoards: { ...state.allBoards } };
-      delete newState.allBoards[action.boardId];
-      return newState;
+      const newAllBoards = { ...state.allBoards };
+      delete newAllBoards[action.boardId];
+      return {
+        ...state,
+        oneBoard: {},
+        allBoards: newAllBoards,
+      };
     }
 
     default: {
